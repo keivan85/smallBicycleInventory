31864e941ca808414a0ace22b142cfc395386c70
keivan85 <keyvankarimi.kk@gmail.com>
2021-01-16 20:46:54 +0200

    Add pagination for showing bicycles on front page

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -1,19 +1,20 @@
 <!doctype html>
 
 <html lang="en">
-  <head>
-    <title>Chain Gang <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/public.css'); ?>" />
-  </head>
 
-  <body>
+<head>
+  <title>Chain Gang <?php if (isset($page_title)) {
+                      echo '- ' . h($page_title);
+                    } ?></title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/public.css'); ?>" />
+</head>
 
-    <header>
-      <h1>
-        <a href="<?php echo url_for('/index.php'); ?>">
-          <img class="bike-icon" src="<?php echo url_for('/images/USDOT_bicycle_symbol.svg') ?>" /><br />
-          Chain Gang
-        </a>
-      </h1>
-    </header>
+<body>
+
+  <header>
+    <a href="<?php echo url_for('/index.php'); ?>">
+      <img class="bike-icon" src="<?php echo url_for('/images/USDOT_bicycle_symbol.svg') ?>" /><br />
+    </a>
+    <h2>Chain Gang</h2>
+  </header>
